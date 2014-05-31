519fa9cb31446e9821beca0723d9e1a685d13fc5
Radu <radumvlad@yahoo.com>
2014-05-31 18:40:18 +0300

    heroku db--test

--- app/controllers/test.php
+++ app/controllers/test.php
@@ -10,4 +10,14 @@ class Test extends CI_Controller {
         die("test");
     }
 
+
+    public function db(){
+    	$heroku_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+
+    	echo $heroku_url["host"] . '<br>';
+    	echo $heroku_url["user"] . '<br>';
+    	echo $heroku_url["pass"] . '<br>';
+    	echo substr($heroku_url["path"],1) . '<br>';
+    }
 }
